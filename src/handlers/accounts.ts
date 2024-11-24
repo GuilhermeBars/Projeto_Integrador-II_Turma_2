26462b4d4d97ef77aea3ec20fb92977661b8fe96
caiomedeiros1 <cadu2005medeiros@gmail.com>
2024-11-24 17:20:22 -0300

    signUp corrigido

--- src/handlers/accounts.ts
+++ src/handlers/accounts.ts
@@ -90,25 +90,61 @@ export namespace AccountsHandler {
         }
     }
 
+    
     export const createAccountRoute: RequestHandler = async (req: Request, res: Response) => {
-        const pName = req.get('name'); 
-        const pEmail = req.get('email'); 
+        const pName = req.get('name');
+        const pEmail = req.get('email');
         const pPassword = req.get('password');
         const pUser = "user";
+        const pBirthdate = req.get('birthdate');
 
-        if (pName && pEmail && pPassword) {
+        if (pName && pEmail && pPassword && pBirthdate) {
+            const user_year = Number(pBirthdate.substring(0, 4)); // Ano
+            const user_month = Number(pBirthdate.substring(5, 7)); // Mês
+            const user_day = Number(pBirthdate.substring(8, 10)); // Dia
+            
+            console.log(user_year);
+            console.log(user_month);
+            console.log(user_day);
+            
+            const today = new Date();
+            const today_year = today.getFullYear();
+            const today_month = today.getMonth() + 1; // Mes atual (0-indexed, então somamos 1)
+            const today_day = today.getDate(); // Dia atual
+            
+            const today_date = today_year + "-" + String(today_month).padStart(2, '0') + "-" + String(today_day).padStart(2, '0');
+            console.log(today_date);
+            
+            // Calculando a idade
+            let age = today_year - user_year;
+            
+            // Verificando se o aniversário já passou no ano atual
+            if (today_month < user_month || (today_month === user_month && today_day < user_day)) {
+                age -= 1; // Subtrai 1 se o aniversário ainda não ocorreu
+            }
+            
+            console.log("Idade do usuário:", age);
+            
+            // Verifica se o usuário tem menos de 18 anos
+            if (age < 18) {
+                res.status(403).send("Usuário deve ter pelo menos 18 anos.");
+                return;
+            }
+
+            const birthDate = new Date(pBirthdate);
+
             const emailAvailable = await verifyEmail(pEmail);
 
             if (emailAvailable) {
-                const connection:any = await connectOracle();
+                const connection: any = await connectOracle();
                 OracleDB.outFormat = OracleDB.OUT_FORMAT_OBJECT;
 
                 try {
                     await connection.execute(
-                        "INSERT INTO ACCOUNTS VALUES (SEQ_ACCOUNTS.nextval, :name, :email, :password, :user_type, dbms_random.string('x',32))",
-                        { name: pName, email: pEmail, password: pPassword, user_type: pUser },
+                        "INSERT INTO ACCOUNTS VALUES (SEQ_ACCOUNTS.nextval, :name, :email, :password, :user_type, dbms_random.string('x',32), :birthdate)",
+                        { name: pName, email: pEmail, password: pPassword, user_type: pUser, birthdate: birthDate },
                         { autoCommit: true }
-                    );                    
+                    );
 
                     await connection.execute(
                         "INSERT INTO WALLETS (EMAIL, BALANCE) VALUES (:email, 0)",
@@ -121,7 +157,7 @@ export namespace AccountsHandler {
                 catch (error) {
                     console.error('Erro ao criar conta:', error);
                     res.status(500).send("Erro ao criar conta.");
-                }
+                } 
                 finally {
                     await connection.close();
                 }
@@ -133,5 +169,5 @@ export namespace AccountsHandler {
         else {
             res.status(400).send("Parâmetros inválidos ou faltantes.");
         }
-    }
+    };
 }
